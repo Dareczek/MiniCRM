aafe0329c58d1046b9569b577cdb81537e27382f
Dariusz Mróz <darmro94@outlook.com>
2016-06-08 18:38:01 +0200

    widok i klasy

--- MiniCRM/MiniCRM/MainWindow.xaml.cs
+++ MiniCRM/MiniCRM/MainWindow.xaml.cs
@@ -18,11 +18,16 @@ namespace MiniCRM
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+           
         }
     }
 }
