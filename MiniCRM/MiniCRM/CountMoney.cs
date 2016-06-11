c6475bb8d6f79fa78d8e03c824a712542efb31f3
Dariusz Mróz <darmro94@outlook.com>
2016-06-11 23:36:16 +0200

    dużo zmian

---
+++ MiniCRM/MiniCRM/CountMoney.cs
@@ -0,0 +1,11 @@
+﻿using ModelDataBase;
+
+namespace MiniCRM
+{
+    abstract class CountMoney
+    {
+        protected double _result;
+        protected double[] Factor = {1, 4.38, 3.89};
+        public abstract double Count(CRMEntities crmEntities);
+    }
+}
