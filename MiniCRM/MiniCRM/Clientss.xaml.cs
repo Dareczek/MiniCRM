5b92b0100b970b4dabddf16e682b311fa17d6a2b
Mati <Mati@METALLIC_FIRE>
2016-06-12 14:23:18 +0200

    dsdf

--- MiniCRM/MiniCRM/Clientss.xaml.cs
+++ MiniCRM/MiniCRM/Clientss.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using DataBase;
@@ -8,8 +10,14 @@ namespace MiniCRM
     /// <summary>
     /// Interaction logic for Zamowienia.xaml
     /// </summary>
-    public partial class Clientss
+    public interface INamesClients
     {
+        // interface members
+        string showTransaction(CRMEntities crmEntities);
+    }
+    public partial class Clientss : INamesClients
+    {
+        private string[] Name = { "Klient Detaliczny","Firma" };
         private CRMEntities _crmEntities;
         public Clientss()
         {
@@ -24,6 +32,24 @@ namespace MiniCRM
                 _crmEntities.Clients.Load();
                 zdarzenieViewSource.Source = _crmEntities.Clients.Local;
             }
+        }
+
+        public string showTransaction(CRMEntities crmEntities)
+        {
+            foreach (var counter in crmEntities.Clients)
+            {
+                var nazwa = from p in crmEntities.Clients
+                    where (counter.ClientId == p.ClientId)
+                    select p.Nip;
+                if (counter.Nip != "")
+                    return counter.Nip;
+            }
+            return null;
+        }
+
+        private void CountButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
