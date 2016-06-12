0964115320764e1c24543a619356859283ba81f3
Dariusz Mróz <darmro94@outlook.com>
2016-06-12 11:50:01 +0200

    qwe

---
+++ MiniCRM/MiniCRM/CountIncome.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using ModelDataBase;
+
+namespace MiniCRM
+{
+    class CountIncome : CountMoney
+    {
+
+        private int _currency;
+        public CountIncome(int currency)
+        {
+            _currency = currency;
+        }
+
+        public override double Count(CRMEntities crmEntities)
+        {
+            foreach (var lol in crmEntities.Orderiks)
+            {
+                var price = from p in crmEntities.Products
+                            where (lol.ProductId == p.ProductId)
+                            select p.Price;
+                if (lol.Paid == 0)
+                    _result += lol.Amount.GetValueOrDefault() * price.First().GetValueOrDefault();
+            }
+            _result = Factor[_currency];
+            return _result;
+        }
+    }
+}
